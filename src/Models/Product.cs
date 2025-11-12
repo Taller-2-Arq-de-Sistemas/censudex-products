f62824324d368537ca8b28b72e35a2513eed82e9
Yinkov <yinko.veliz@alumnos.ucn.cl>
2025-11-11 21:45:47 -0300

    feat: add Models

---
+++ src/Models/Product.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace censudex_products.src.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string description { get; set; }  = string.Empty;
+        public decimal Price { get; set; } 
+        public string category { get; set; } = string.Empty;
+        public string imageUrl { get; set; } = string.Empty;
+        public bool isActive { get; set; } = true;
+        public DateTime registrationDate { get; set; } = DateTime.Now;
+
+    }
+}
