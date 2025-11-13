d9deade704c81aca40e22545b2eb1b849005001e
Yinkov <yinko.veliz@alumnos.ucn.cl>
2025-11-13 16:42:11 -0300

    feat: Interfaces y Dtos

---
+++ src/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using censudex_products.src.Dtos;
+using censudex_products.src.Models;
+
+namespace censudex_products.src.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> Add(Product product);
+        Task<List<Product>> GetAll(string? userId, DateTime? fecha);
+        Task<Product> GetById(string id);
+        Task<bool> Update(string id,UpdateProductDto  product);
+        Task<bool> DeleteSoft(string id); 
+    }
+}
--- src/Models/Product.cs
+++ src/Models/Product.cs
@@ -11,8 +11,9 @@ namespace censudex_products.src.Models
         [BsonId]
         [BsonRepresentation(BsonType.String)]
         public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string name { get; set; }  = string.Empty;
         public string description { get; set; }  = string.Empty;
-        public decimal Price { get; set; } 
+        public decimal price { get; set; } 
         public string category { get; set; } = string.Empty;
         public string imageUrl { get; set; } = string.Empty;
         public bool isActive { get; set; } = true;
